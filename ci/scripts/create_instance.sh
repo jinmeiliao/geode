394b282ccc4552e680285b5fc78703ec5dda3f08
Robert Houghton <rhoughton@pivotal.io>
2020-01-06 12:53:45 -0800

    GEODE-7590 - Use GCESysprep as last-step in Windows Packer build. (#4522)
    * Fix instance cleanup per zone.
    * delete_instanch.sh deletes in all zones just in case.
    * Ask for zones, don't assume.
    * Use Packer release 1.4.5, not latest prerelease.
    * Need reboot before running GCESysPrep in case of windows update.
    
    (cherry picked from commit 2178ba8fefcda23d4b2d3b6ecc68b58dd8839f7f)

--- ci/scripts/create_instance.sh
+++ ci/scripts/create_instance.sh
@@ -43,6 +43,18 @@ if [[ -z "${GEODE_BRANCH}" ]]; then
   exit 1
 fi
 
+if [[ -z "${IMAGE_FAMILY_NAME}" ]]; then
+  echo "IMAGE_FAMILY_NAME environment variable must be set for this script to work."
+  exit 1
+fi
+
+if [[ -d geode ]]; then
+  pushd geode
+  GEODE_SHA=$(git rev-parse --verify HEAD)
+  popd
+else
+  GEODE_SHA="unknown"
+fi
 
 . ${SCRIPTDIR}/../pipelines/shared/utilities.sh
 SANITIZED_GEODE_BRANCH=$(getSanitizedBranch ${GEODE_BRANCH})
@@ -78,16 +90,18 @@ cp old/attempts new/
 echo attempt >> new/attempts
 attempts=$(cat new/attempts | wc -l)
 
+PERMITTED_ZONES=($(gcloud compute zones list --filter="name~'us-central.*'" --format=json | jq -r .[].name))
 if [ $attempts -eq 1 ]; then
   ZONE=${MY_ZONE}
 else
-  PERMITTED_ZONES=(us-central1-a us-central1-b us-central1-c us-central1-f)
   ZONE=${PERMITTED_ZONES[$((${RANDOM} % 4))]}
 fi
 echo "Deploying to zone ${ZONE}"
 
-#in a retry loop we intentionally generate the same instance name, so make sure prior attempt is cleaned up
-gcloud compute instances delete ${INSTANCE_NAME} --zone=${ZONE} --quiet &>/dev/null || true
+# Ensure no existing instance with this name in any zone
+for KILL_ZONE in $(echo ${PERMITTED_ZONES[*]}); do
+  gcloud compute instances delete ${INSTANCE_NAME} --zone=${KILL_ZONE} --quiet &>/dev/null || true
+done
 
 echo "${INSTANCE_NAME}" > "instance-data/instance-name"
 echo "${GCP_PROJECT}" > "instance-data/project"
@@ -96,16 +110,18 @@ echo "${ZONE}" > "instance-data/zone"
 echo 'StrictHostKeyChecking no' >> /etc/ssh/ssh_config
 RAM_MEGABYTES=$( expr ${RAM} \* 1024 )
 
-TTL=$(($(date +%s) + 60 * 60 * 6))
-LABELS="instance_type=heavy-lifter,time-to-live=${TTL},job-name=${SANITIZED_BUILD_JOB_NAME},pipeline-name=${SANITIZED_BUILD_PIPELINE_NAME},build-name=${SANITIZED_BUILD_NAME}"
+TTL=$(($(date +%s) + 60 * 60 * 12))
+LABELS="instance_type=heavy-lifter,time-to-live=${TTL},job-name=${SANITIZED_BUILD_JOB_NAME},pipeline-name=${SANITIZED_BUILD_PIPELINE_NAME},build-name=${SANITIZED_BUILD_NAME},sha=${GEODE_SHA}"
+echo "Applying the following labels to the instance: ${LABELS}"
 
+set +e
 INSTANCE_INFORMATION=$(gcloud compute --project=${GCP_PROJECT} instances create ${INSTANCE_NAME} \
   --zone=${ZONE} \
   --machine-type=custom-${CPUS}-${RAM_MEGABYTES} \
   --min-cpu-platform=Intel\ Skylake \
   --network="${GCP_NETWORK}" \
   --subnet="${GCP_SUBNETWORK}" \
-  --image-family="${IMAGE_FAMILY_PREFIX}${WINDOWS_PREFIX}geode-builder" \
+  --image-family="${IMAGE_FAMILY_NAME}" \
   --boot-disk-size=100GB \
   --boot-disk-type=pd-ssd \
   --labels="${LABELS}" \
@@ -113,9 +129,21 @@ INSTANCE_INFORMATION=$(gcloud compute --project=${GCP_PROJECT} instances create 
   --scopes="default,storage-rw" \
   --format=json)
 
+CREATE_RC=$?
+set -e
+
+if [[ ${CREATE_RC} -ne 0 ]]; then
+  sleep 30
+  exit 1
+fi
+
 echo "${INSTANCE_INFORMATION}" > instance-data/instance-information
 
 INSTANCE_IP_ADDRESS=$(echo ${INSTANCE_INFORMATION} | jq -r '.[].networkInterfaces[0].networkIP')
+INSTANCE_ID=$(echo ${INSTANCE_INFORMATION} | jq -r '.[].id')
+
+echo "Heavy lifter's Instance ID is: ${INSTANCE_ID}"
+
 echo "${INSTANCE_IP_ADDRESS}" > "instance-data/instance-ip-address"
 
 if [[ -z "${WINDOWS_PREFIX}" ]]; then
@@ -132,10 +160,25 @@ if [[ -z "${WINDOWS_PREFIX}" ]]; then
 else
   # Set up ssh access for Windows systems
   echo -n "Setting windows password via gcloud."
-  while [[ -z "${PASSWORD}" ]]; do
+  INSTANCE_AGENT_READY_LINE="GCEWindowsAgent: GCE Agent Started"
+  INSTANCE_SETUP_FINSHED_LINE="GCEInstanceSetup: Instance setup finished"
+  SCRAPE_COMMAND_INSTANCE_READY="gcloud compute instances get-serial-port-output ${INSTANCE_NAME} --zone=${ZONE} | grep \"${INSTANCE_AGENT_READY_LINE}\" | wc -l"
+  SCRAPE_COMMAND_SETUP_FINSHED="gcloud compute instances get-serial-port-output ${INSTANCE_NAME} --zone=${ZONE} | grep \"${INSTANCE_SETUP_FINSHED_LINE}\" | wc -l"
+
+  while true; do
+    # Check that the instance agent has started at least 2x (first boot, plus activation)
+    # and that the "GCEInstanceSetup" script completed
+    echo -n "Waiting for startup scripts and windows activation to complete"
+    while [[ 2 -ne $(eval ${SCRAPE_COMMAND_INSTANCE_READY} 2> /dev/null) ]] || [[ 1 -ne $(eval ${SCRAPE_COMMAND_SETUP_FINSHED} 2> /dev/null) ]]; do
+      echo -n .
+      sleep 5
+    done
+    echo ""
+    # Get a password
     PASSWORD=$( yes | gcloud beta compute reset-windows-password ${INSTANCE_NAME} --user=geode --zone=${ZONE} --format json | jq -r .password )
-    echo -n .
-    sleep 5
+    if [[ -n "${PASSWORD}" ]]; then
+      break;
+    fi
   done
 
   ssh-keygen -N "" -f ${SSHKEY_FILE}
