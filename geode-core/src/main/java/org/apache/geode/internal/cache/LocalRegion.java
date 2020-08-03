60cd9f313c5aabe74391f3a0d8c930bfd7f8f061
Darrel Schneider <dschneider@pivotal.io>
2020-08-03 14:37:39 -0700

    GEODE-6564: fix entryExpiryTasks leak (#5419)
    (cherry picked from commit 53abb22cd295441f8794e15550b72ec81113ec05)

--- geode-core/src/main/java/org/apache/geode/internal/cache/LocalRegion.java
+++ geode-core/src/main/java/org/apache/geode/internal/cache/LocalRegion.java
@@ -7919,7 +7919,10 @@ public class LocalRegion extends AbstractRegion implements LoaderHelperFactory,
       return;
     }
     boolean doPurge = false;
-    for (EntryExpiryTask task : entryExpiryTasks.values()) {
+    Iterator<EntryExpiryTask> iterator = entryExpiryTasks.values().iterator();
+    while (iterator.hasNext()) {
+      EntryExpiryTask task = iterator.next();
+      iterator.remove();
       // no need to call incCancels since we will call forcePurge
       task.cancel();
       doPurge = true;
@@ -11247,4 +11250,10 @@ public class LocalRegion extends AbstractRegion implements LoaderHelperFactory,
       }
     }
   }
+
+  @VisibleForTesting
+  ConcurrentHashMap<RegionEntry, EntryExpiryTask> getEntryExpiryTasks() {
+    return entryExpiryTasks;
+  }
+
 }
