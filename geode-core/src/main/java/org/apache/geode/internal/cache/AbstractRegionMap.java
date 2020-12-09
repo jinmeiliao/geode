e2e68353667a53c0ddd1b1fc7cdff45b60be7dc3
Jinmei Liao <jiliao@pivotal.io>
2020-12-09 10:04:51 -0800

    lock before indexInit

--- geode-core/src/main/java/org/apache/geode/internal/cache/AbstractRegionMap.java
+++ geode-core/src/main/java/org/apache/geode/internal/cache/AbstractRegionMap.java
@@ -1277,6 +1277,10 @@ public abstract class AbstractRegionMap extends BaseRegionMap
     DiskRegion dr = owner.getDiskRegion();
     boolean ownerIsInitialized = owner.isInitialized();
 
+    // lock before waitForIndexInit so that we should wait
+    // till a concurrent clear to finish
+    lockForCacheModification(owner, event);
+
     // Fix for Bug #44431. We do NOT want to update the region and wait
     // later for index INIT as region.clear() can cause inconsistency if
     // happened in parallel as it also does index INIT.
@@ -1284,7 +1288,7 @@ public abstract class AbstractRegionMap extends BaseRegionMap
     if (oqlIndexManager != null) {
       oqlIndexManager.waitForIndexInit();
     }
-    lockForCacheModification(owner, event);
+
     final boolean locked = owner.lockWhenRegionIsInitializing();
     try {
       try {
